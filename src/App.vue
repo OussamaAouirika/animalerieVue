680c6eeed53e8a9bde73465b4105e341d772ca6f
Oussama Aouirika <oussama.aouirika@student.ichec.be>
2020-05-03 19:48:23 +0200

    indent + colors

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,50 @@
-
-
 <template>
   <div id="app">
     <h1 id="myHeader">Mon animalerie</h1>
     <div class="conteneur-flex">
-    <Listing></Listing>
-    <Add></Add>
+      <Listing></Listing>
+      <Add></Add>
     </div>
   </div>
 </template>
 
 <script>
-import Listing from './Listing'
-import Add from './Add'
-import tuto from './tuto'
+import Listing from "./Listing";
+import Add from "./Add";
+import tuto from "./tuto";
 export default {
-  name: 'app',
-  components : {Listing,Add,tuto},
-  data () {
+  name: "app",
+  components: { Listing, Add, tuto },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #000000;
   margin-top: 60px;
-
 }
-#myHeader
-{
+#myHeader {
   font-size: 48px;
   text-shadow: -1px -1px #9df, 1px 1px #49d, -3px 0 4px #000;
-  font-family:"Segoe print", Arial, Helvetica, sans-serif;
-  color:#6bf;
-  padding:24px 32px 32px 32px;
-  font-weight:lighter;
-  -moz-box-shadow: 2px 2px 6px #888;  
-  -webkit-box-shadow: 2px 2px 6px #888;  
-  box-shadow:2px 2px 6px #888;  
-  text-align:center;
-  display:inline;
-  line-height:150px;
-  background-image:url(pic.jpg);
+  font-family: "Segoe print", Arial, Helvetica, sans-serif;
+  color: #6bf;
+  padding: 24px 32px 32px 32px;
+  font-weight: lighter;
+  -moz-box-shadow: 2px 2px 6px #888;
+  -webkit-box-shadow: 2px 2px 6px #888;
+  box-shadow: 2px 2px 6px #888;
+  text-align: center;
+  display: inline;
+  line-height: 150px;
+  background-image: url(pic.jpg);
 }
-
-
-
 </style>
--- src/Listing.vue
+++ src/Listing.vue
@@ -1,173 +1,188 @@
-
-
 <template>
-
-  
   <div id="Listing">
-  
-<body>  
-        
-           <div class="container" id="tuto">
-      <br>
- 
-      <div class="panel panel-primary">
-        <div class="panel-heading">Listing de nos Animaux</div> 
-         <table class="table table-fixed" >
-                    <thead>
-                        <tr>
-                            <th>Nom de l'animal</th>
-                            <th>Age de l'animal</th>
-                            <th>Poids de l'animal (en KG)</th>
-                            <th>Règne animal</th>
-                            <th>Provenance animal</th>
-                            <th>Espace Vital animal</th>
-                            <th>Nom propriétaire</th>
-                        </tr>
-                    </thead>
-                    <tbody >
-                       <tr v-for= "animal in animals">
-                            <td>
-                                {{animal.nom}}
-                            </td>
-                            <td>
-                                {{animal.age}}
-                            </td>
-                             <td>
-                                {{animal.poids}}
-                            </td>
-                            <td>
-                                {{animal.nomregne}}
-                            </td>
-                            <td>
-                                {{animal.provenance}}
-                            </td>
-                            <td>
-                                {{animal.espace}}
-                            </td>
-                            <td>
-                                {{animal.login}}
-                            </td>
-                        </tr>
-                    </tbody>
-        </table>
-         </div>
-         </div>
-        
-         </body>
-         
+    <body>
+      <div class="container" id="tuto">
+        <br />
+
+        <div class="panel panel-primary">
+          <div class="panel-heading">Listing de nos Animaux</div>
+          <table class="table table-fixed">
+            <thead>
+              <tr>
+                <th>Nom de l'animal</th>
+                <th>Age de l'animal</th>
+                <th>Poids de l'animal (en KG)</th>
+                <th>Règne animal</th>
+                <th>Provenance animal</th>
+                <th>Espace Vital animal</th>
+                <th>Nom propriétaire</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="animal in animals">
+                <td>
+                  {{ animal.nom }}
+                </td>
+                <td>
+                  {{ animal.age }}
+                </td>
+                <td>
+                  {{ animal.poids }}
+                </td>
+                <td>
+                  {{ animal.nomregne }}
+                </td>
+                <td>
+                  {{ animal.provenance }}
+                </td>
+                <td>
+                  {{ animal.espace }}
+                </td>
+                <td>
+                  {{ animal.login }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </body>
   </div>
-  
 </template>
 
-
-<script> 
-import axios from 'axios';
+<script>
+import axios from "axios";
 export default {
-  name: 'listing',
-  data () {
+  name: "listing",
+  data() {
     return {
-      animals:[],
-      url: 'http://localhost:8080/api/animals'
-    }
+      animals: [],
+      url: "http://localhost:8080/api/animals"
+    };
   },
 
   methods: {
     get_animals() {
       axios
-        .get('http://localhost:8080/api/animals')
-        .then( response=> { 
-            this.animals = response.data.animals;
-            console.log(this.animals);
+        .get("http://localhost:8080/api/animals")
+        .then(response => {
+          this.animals = response.data.animals;
+          console.log(this.animals);
         })
-        .catch((error) => {
-            console.log(error);
-        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   },
   mounted() {
-        this.get_animals();
+    this.get_animals();
   }
-}
+};
 </script>
 
 <style>
-
-
 table {
-border:3px solid #6495ed;
-border-collapse:collapse;
-width:90%;
-margin:auto;
+  border: 3px solid #6495ed;
+  border-collapse: collapse;
+  width: 90%;
+  margin: auto;
 }
-thead, tfoot {
-background-color:#D0E3FA;
-border:1px solid #6495ed;
+thead,
+tfoot {
+  background-color: #d0e3fa;
+  border: 1px solid #6495ed;
 }
 tbody {
-background-color:#FFFFFF;
-border:1px solid #6495ed;
+  background-color: #ffffff;
+  border: 1px solid #6495ed;
 }
 th {
-font-family:monospace;
-border:1px dotted #6495ed;
-padding:5px;
-background-color:#EFF6FF;
-width:25%;
+  font-family: monospace;
+  border: 1px dotted #6495ed;
+  padding: 5px;
+  background-color: #eff6ff;
+  width: 25%;
 }
 td {
-font-family:sans-serif;
-font-size:80%;
-border:1px solid #6495ed;
-padding:5px;
-text-align:left;
+  font-family: sans-serif;
+  font-size: 80%;
+  border: 1px solid #6495ed;
+  padding: 5px;
+  text-align: left;
 }
 caption {
-font-family:sans-serif;
+  font-family: sans-serif;
 }
 
-@media 
-only screen and (max-width: 760px),
-(min-device-width: 768px) and (max-device-width: 1024px)  {
+@media only screen and (max-width: 760px),
+  (min-device-width: 768px) and (max-device-width: 1024px) {
+  /* Force table to not be like tables anymore */
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
 
-	/* Force table to not be like tables anymore */
-	table, thead, tbody, th, td, tr { 
-		display: block; 
-	}
-	
-	/* Hide table headers (but not display: none;, for accessibility) */
-	thead tr { 
-		position: absolute;
-		top: -9999px;
-		left: -9999px;
-	}
-	
-	tr { border: 1px solid #ccc; }
-	
-	td { 
-		/* Behave  like a "row" */
-		border: none;
-		border-bottom: 1px solid #eee; 
-		position: relative;
-		padding-left: 50%; 
-	}
-	
-	td:before { 
-		/* Now like a table header */
-		position: absolute;
-		/* Top/left values mimic padding */
-		top: 6px;
-		left: 6px;
-		width: 45%; 
-		padding-right: 10px; 
-		white-space: nowrap;
-	}
+  /* Hide table headers (but not display: none;, for accessibility) */
+  thead tr {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
 
-  td:nth-of-type(1):before { content: "Nom Animal"; font-weight: bold; }
-	td:nth-of-type(2):before { content: "Age Animal"; font-weight: bold; }
-	td:nth-of-type(3):before { content: "Poids Animal"; font-weight: bold;  }
-	td:nth-of-type(4):before { content: "Regne Animal"; font-weight: bold; }
-	td:nth-of-type(5):before { content: "Provenance"; font-weight: bold; }
-	td:nth-of-type(6):before { content: "Espace Vital"; font-weight: bold; }
-	td:nth-of-type(7):before { content: "Nom propriétaire"; font-weight: bold; }
+  tr {
+    border: 3px solid #ccc;
+  }
+
+  td {
+    /* Behave  like a "row" */
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 50%;
+  }
+
+  td:before {
+    /* Now like a table header */
+    position: absolute;
+    /* Top/left values mimic padding */
+    top: 6px;
+    left: 6px;
+    width: 45%;
+    padding-right: 10px;
+    white-space: nowrap;
+  }
+
+  td:nth-of-type(1):before {
+    content: "Nom Animal";
+    font-weight: bold;
+  }
+  td:nth-of-type(2):before {
+    content: "Age Animal";
+    font-weight: bold;
+  }
+  td:nth-of-type(3):before {
+    content: "Poids Animal";
+    font-weight: bold;
+  }
+  td:nth-of-type(4):before {
+    content: "Regne Animal";
+    font-weight: bold;
+  }
+  td:nth-of-type(5):before {
+    content: "Provenance";
+    font-weight: bold;
+  }
+  td:nth-of-type(6):before {
+    content: "Espace Vital";
+    font-weight: bold;
+  }
+  td:nth-of-type(7):before {
+    content: "Nom propriétaire";
+    font-weight: bold;
+  }
 }
 </style>
